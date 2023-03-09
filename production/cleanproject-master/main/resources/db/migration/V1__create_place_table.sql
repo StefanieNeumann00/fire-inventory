3726c1deced239b7843c76198f9fc2c5814c1f77
StefanieNeumann00 <stefanie.neumann.ka@outlook.de>
2023-03-09 13:35:36 +0100

    deleted production folder

--- production/cleanproject-master/main/resources/db/migration/V1__create_place_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE place (
-    id          uuid          not null,
-    designation       varchar    not null,
-    constraint pk_place primary key (id)
-);
--- production/cleanproject-master/main/resources/db/migration/V1__create_vehicle_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE vehicle (
-    id          uuid          not null,
-    designation       varchar    not null,
-    status_id    uuid    not null,
-    place_id    uuid    not null,
-    constraint pk_vehicle primary key (id),
-    constraint fk_vehiclestatus foreign key (status_id) references Status(id),
-    constraint fk_vehicleplace foreign key (place_id) references Place(id)
-);
